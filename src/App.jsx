cee389839e747c43b6fb91302cb015badfd22139
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-01-17 09:56:27 +0300

    setup login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Login } from './pages/Login';
+import { Container } from '@mui/material';
+
 function App() {
-	return <>App</>;
+	return (
+		<Container component="main">
+			<Routes>
+				<Route path="/" element={<>Home</>} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</Container>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter } from 'react-router-dom';
+import theme from './theme.js';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<ThemeProvider>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>
 );
